bc67b497f1bf5eb7165addf8b8dea0c67146a93a
clouduser99 <41755692+clouduser99@users.noreply.github.com>
2020-04-20 16:39:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,1 @@
-print('Hello, world and testing of WebHooks and it finally worked after testing in New Jenkins Server and now testing the multibranch master!')
+print('Hello, world and testing of Multibranch pipeline for webhooks!')
